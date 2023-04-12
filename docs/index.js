5b6826145c84499010f7b958b8efd0ebb9e2909b
garrepi <jacgarrett18@gmail.com>
2023-04-12 13:48:33 -0400

    remove registration functions

--- docs/index.js
+++ docs/index.js
@@ -29,41 +29,6 @@ function track_view() {
 
 }
 
-function fetch_guests() {
-    fetch("https://drop1.garrepi.dev/guests")
-        .then(res => res.json())
-        .then(guests => {
-            const nodes = guests
-                .map(guest => `${guest.name}`)
-                .map(name => {
-                    const p = document.createElement("p");
-                    const t = document.createTextNode(name);
-                    p.appendChild(t);
-
-                    return p;
-                });
-
-            document.getElementById("guests").replaceChildren(...nodes);
-        })
-        .catch(err => {
-            console.log(err);
-        });
-}
-
-function fetch_guest_count() {
-    fetch("https://drop1.garrepi.dev/guest-count")
-        .then(res => res.json())
-        .then(count => {
-            const guestCounter = document.getElementById("guest-count");
-            guestCounter.textContent = count + " people registered";
-        })
-        .catch(err => {
-            const guestCounter = document.getElementById("guest-count");
-            guestCounter.textContent = "Error fetching guest count";
-            console.log('guest count: ', err);
-        });
-}
-
 function fetch_view_count() {
     fetch("https://drop1.garrepi.dev/view-count")
         .then(res => res.json())
@@ -124,7 +89,5 @@ function register() {
 }
 
 track_view();
-//fetch_guests();
-//fetch_guest_count();
 fetch_view_count();
 poll_registration_status();
